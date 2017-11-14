9d76431bb8cecd654150a4e6ae05a847d67610cc
Lance <Lance>
2017-11-14 11:47:39 +0200

    fixed real-ip header bug

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if ! [ -z ${DOWNSTREAM_HOSTS+x} ]; then
+if ! [ -z ${DOWNSTREAM_HOSTS+x} ] && ! [ -e /etc/nginx/conf.d/realip.conf ] &&; then
 
   IFS=', ' read -r -a array <<< "${DOWNSTREAM_HOSTS}"
   echo '# Downstream hosts (real-ip)' >> /etc/nginx/conf.d/realip.conf;
 
   for element in "${array[@]}"
   do
-      echo "set_real_ip_from $element" >> /etc/nginx/conf.d/realip.conf;
+      echo "set_real_ip_from $element;" >> /etc/nginx/conf.d/realip.conf;
   done
 
   echo "real_ip_header ${REAL_IP_HEADER}" >> /etc/nginx/conf.d/realip.conf;
